02d3c4a31e2153473cfd9d7d09b11db6bb1168a5
epenet <6771947+epenet@users.noreply.github.com>
2026-02-11 07:35:52 +0000

    Combine matter snapshot tests

--- tests/components/matter/common.py
+++ tests/components/matter/common.py
@@ -18,6 +18,91 @@ from homeassistant.helpers import entity_registry as er
 
 from tests.common import MockConfigEntry, load_fixture
 
+FIXTURES = [
+    "air_quality_sensor",
+    "aqara_door_window_p2",
+    "aqara_motion_p2",
+    "aqara_presence_fp300",
+    "aqara_sensor_w100",
+    "aqara_thermostat_w500",
+    "aqara_u200",
+    "color_temperature_light",
+    "ecovacs_deebot",
+    "eberle_ute3000",
+    "eufy_vacuum_omni_e28",
+    "eve_contact_sensor",
+    "eve_energy_20ecn4101",
+    "eve_energy_plug",
+    "eve_energy_plug_patched",
+    "eve_shutter",
+    "eve_thermo_v4",
+    "eve_thermo_v5",
+    "eve_weather_sensor",
+    "extended_color_light",
+    "haojai_switch",
+    "heiman_co_sensor",
+    "heiman_motion_sensor_m1",
+    "heiman_smoke_detector",
+    "ikea_air_quality_monitor",
+    "ikea_scroll_wheel",
+    "inovelli_vtm30",
+    "inovelli_vtm31",
+    "longan_link_thermostat",
+    "mock_air_purifier",
+    "mock_battery_storage",
+    "mock_cooktop",
+    "mock_dimmable_light",
+    "mock_dimmable_plugin_unit",
+    "mock_door_lock",
+    "mock_door_lock_with_unbolt",
+    "mock_extractor_hood",
+    "mock_fan",
+    "mock_flow_sensor",
+    "mock_generic_switch",
+    "mock_generic_switch_multi",
+    "mock_humidity_sensor",
+    "mock_laundry_dryer",
+    "mock_leak_sensor",
+    "mock_light_sensor",
+    "mock_lock",
+    "mock_microwave_oven",
+    "mock_mounted_dimmable_load_control_fixture",
+    "mock_occupancy_sensor",
+    "mock_on_off_plugin_unit",
+    "mock_onoff_light",
+    "mock_onoff_light_alt_name",
+    "mock_onoff_light_no_name",
+    "mock_oven",
+    "mock_pressure_sensor",
+    "mock_pump",
+    "mock_room_airconditioner",
+    "mock_solar_inverter",
+    "mock_speaker",
+    "mock_switch_unit",
+    "mock_temperature_sensor",
+    "mock_thermostat",
+    "mock_valve",
+    "mock_vacuum_cleaner",
+    "mock_window_covering_full",
+    "mock_window_covering_lift",
+    "mock_window_covering_pa_lift",
+    "mock_window_covering_pa_tilt",
+    "mock_window_covering_tilt",
+    "onoff_light_with_levelcontrol_present",
+    "resideo_x2s_thermostat",
+    "secuyou_smart_lock",
+    "silabs_dishwasher",
+    "silabs_evse_charging",
+    "silabs_laundrywasher",
+    "silabs_light_switch",
+    "silabs_refrigerator",
+    "silabs_water_heater",
+    "switchbot_k11_plus",
+    "tado_smart_radiator_thermostat_x",
+    "yandex_smart_socket",
+    "zemismart_mt25b",
+]
+
 
 @cache
 def load_node_fixture(fixture: str) -> str:
@@ -30,16 +115,21 @@ def load_and_parse_node_fixture(fixture: str) -> dict[str, Any]:
     return json.loads(load_node_fixture(fixture))
 
 
-async def setup_integration_with_node_fixture(
+async def _setup_integration_with_nodes(
     hass: HomeAssistant,
-    node_fixture: str,
     client: MagicMock,
-    override_attributes: dict[str, Any] | None = None,
+    nodes: list[MatterNode],
 ) -> MatterNode:
-    """Set up Matter integration with fixture as node."""
-    node = create_node_from_fixture(node_fixture, override_attributes)
-    client.get_nodes.return_value = [node]
-    client.get_node.return_value = node
+    """Set up Matter integration with nodes."""
+    client.get_nodes.return_value = nodes
+
+    def _get_node(node_id: int) -> MatterNode:
+        try:
+            next(node for node in nodes if node.node_id == node_id)
+        except StopIteration as err:
+            raise KeyError(f"Node with id {node_id} not found") from err
+
+    client.get_node.side_effect = _get_node
     config_entry = MockConfigEntry(
         domain="matter", data={"url": "http://mock-matter-server-url"}
     )
@@ -48,14 +138,47 @@ async def setup_integration_with_node_fixture(
     assert await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
 
+
+async def setup_integration_with_node_fixture(
+    hass: HomeAssistant,
+    node_fixture: str,
+    client: MagicMock,
+    override_attributes: dict[str, Any] | None = None,
+) -> MatterNode:
+    """Set up Matter integration with single fixture as node."""
+    node = create_node_from_fixture(node_fixture, override_attributes)
+
+    await _setup_integration_with_nodes(hass, client, [node])
+
     return node
 
 
+async def setup_integration_with_node_fixtures(
+    hass: HomeAssistant,
+    client: MagicMock,
+) -> None:
+    """Set up Matter integration with all fixtures as nodes."""
+    nodes = [
+        create_node_from_fixture(node_fixture, node_id=next_id)
+        for next_id, node_fixture in enumerate(FIXTURES, start=1)
+    ]
+
+    await _setup_integration_with_nodes(hass, client, nodes)
+
+
 def create_node_from_fixture(
-    node_fixture: str, override_attributes: dict[str, Any] | None = None
+    node_fixture: str,
+    override_attributes: dict[str, Any] | None = None,
+    node_id: int | None = None,
 ) -> MatterNode:
     """Create a node from a fixture."""
     node_data = load_and_parse_node_fixture(node_fixture)
+    if node_id is not None:
+        # Override node_id to ensure uniqueness across fixtures
+        node_data["node_id"] = node_id
+        # Override serial number to ensure uniqueness across fixtures
+        if "0/40/15" in node_data["attributes"]:
+            node_data["attributes"]["0/40/15"] = f"serial_{node_id}"
     if override_attributes:
         node_data["attributes"].update(override_attributes)
     return MatterNode(
@@ -104,5 +227,11 @@ def snapshot_matter_entities(
     entities = hass.states.async_all(platform)
     for entity_state in entities:
         entity_entry = entity_registry.async_get(entity_state.entity_id)
-        assert entity_entry == snapshot(name=f"{entity_entry.entity_id}-entry")
-        assert entity_state == snapshot(name=f"{entity_entry.entity_id}-state")
+        node_id = int(entity_entry.unique_id.split("-")[1], 16)
+        fixture_name = FIXTURES[node_id - 1]
+        assert entity_entry == snapshot(
+            name=f"{fixture_name}-{entity_entry.entity_id}-entry"
+        )
+        assert entity_state == snapshot(
+            name=f"{fixture_name}-{entity_entry.entity_id}-state"
+        )
